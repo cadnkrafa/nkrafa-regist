2e3209e384f60489e1619a0bd62b33ac3f0ec624
cadnkrafa <cad.nkrafa@gmail.com>
2025-05-03 01:16:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzalGvFEc7mIi3TlVCpvE9evGnCrBcfB5Vp5EcBoxXU1xLm4f27joxtfeTaPwVBsowS_w/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0-tH2aquS_VWs_xs8N4jFXMK37326PNVlvqBnxf6ZC-B5X9oPUzPP_ex-MQEfxfha4A/exec';
