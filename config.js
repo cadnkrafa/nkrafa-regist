11070246659c0b712480598131d7fb9309dbcbb5
cadnkrafa <cad.nkrafa@gmail.com>
2025-05-05 21:36:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwIP8bW4tt7XUyqKgJ2gJ-ELkSBVVFiJP2QztXDiJXtAznOKrnyI3HRNvl_Cr-jPtOYdQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwuwuMuwfmi9d-kSRzxR3lj0wsANr3xN25iz7reUMVcOo2znK9gwEMB9w9CO2RrtlVZuA/exec';
 const LIFF_ID_QR = '2007226288-k77xbmX7';
 const LIFF_ID_SCAN = '2007226288-Qj0NDKY0';
+const LIFF_ID_SUPERADMIN = '2007226288-bNnjgPkn';
