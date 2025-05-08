ed2433bce0e87d301abb6e3163b143940498e6f4
cadnkrafa <cad.nkrafa@gmail.com>
2025-05-09 03:30:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0oEpOzwJrLo1bVTfZ3lDG8Cu1RzdyKoMGe5uI00ta8RAbALGekZFoauWyc6ic6DLHQQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby-XGByL9LqcBNuB5sG6c9a2VUUC0vD-Iiy4aVxdlfBi_u9G9cKT-wGhFu0n_G7uZFURQ/exec';
 const LIFF_ID_QR = '2007226288-k77xbmX7';
 const LIFF_ID_SCAN = '2007226288-Qj0NDKY0';
 const LIFF_ID_SUPERADMIN = '2007226288-bNnjgPkn';
